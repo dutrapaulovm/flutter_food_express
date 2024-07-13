b1058e1e39c1d2cc16db4a3d29d407f0c501438a
paulovmdutra <121131923+dutrapaulovm@users.noreply.github.com>
2024-07-13 12:22:56 -0300

    Update Form Estado

--- lib/presentation/screen/estado/estado_form_screen.dart
+++ lib/presentation/screen/estado/estado_form_screen.dart
@@ -11,20 +11,88 @@ class EstadoFormScreen extends StatefulWidget {
 
   @override
   _EstadoFormScreenState createState() => _EstadoFormScreenState();
+
 }
 
 class _EstadoFormScreenState extends State<EstadoFormScreen> {
+  
+  /*Utilizado para validar, resetar ou salvar os dados associar
+  ao Widget Form*/
+  final GlobalKey<FormState> _formState = GlobalKey<FormState>();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {            
+            
+            if (!_formState.currentState!.validate()){
+              showDialog(context: context, 
+                    /*Aqui construimos a janela de diálogo */
+                    builder: (context) {
+                      return AlertDialog(
+                        title : const Text("Aviso!"),
+                        content: 
+                        const Text("Preencha os campos obrigatórios!"),
+                        actions: [
+                            TextButton(onPressed: () {
+                              Navigator.pop(context);
+                            }, child: const Text("OK"))],
+
+                      )
+                      
+                      ;
+                    },);
+            }
+                        
+            //Navigator.pop(context);
+
+          }, icon: const Icon(Icons.check)),
+          IconButton(onPressed: () {
+            Navigator.pop(context);
+          }, icon: const Icon(Icons.cancel),)
+        ],        
         title: const Text('Cadastro Estado'),
       ),
-      body: Container(),
+      body: _body(context),
     );
   }
 
-  
+  Widget _body(BuildContext context){
+    return Padding(padding: const EdgeInsets.all(16),
+      child: Form(   
+      key: _formState,
+        child: Column(children: [
+        Row(children: [
+           Flexible(child: 
+              TextFormField(
+                decoration: InputDecoration(label: Text("Nome")),
+                validator: (value) {
+                  if (value != null && value.trim().isEmpty){
+                     return "Campo obrigatório!";
+                  } 
+                },
+              ), 
+           ) 
+        ],),
+        const SizedBox(height: 16,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [                      
+           Flexible(child: TextFormField(), ),
+           const SizedBox(width: 16,),
+           Flexible(child: TextFormField(), )
 
+           /*SizedBox(width: 100, child: Flexible(child: TextFormField(), )), 
+           const SizedBox(height: 16,),
+           SizedBox(width: 100, child: Flexible(child: TextFormField(), ))*/
+        ],)
+      ]), 
+    
+      )
+    
+    );
+  }
 
 }
